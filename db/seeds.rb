c45e5bd1c629cdc33f16c1d04d83084625c2338b
Bartosz Suchodolski <bartosz.suchodolski@netguru.pl>
2019-11-26 15:54:20 +0100

    Add basic seeds for users and household accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'ffaker'
+
+# Create users
+
+john_doe = User.create(email: 'john_doe@example.com', password: 'password')
+User.create(email: 'johnatan_pear@example.com', password: 'password')
+
+# Create household accounts
+
+account = HouseholdAccount.create(name: FFaker::Company.name)
+account.users << john_doe
