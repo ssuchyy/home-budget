1fcd15360f13e7d7c8ca744201e02b018e5aa00c
Bartosz Suchodolski <bartosz.suchodolski@netguru.pl>
2019-11-27 15:29:55 +0100

    Remove unused shared context param

--- spec/support/shared_contexts/authentication_shared_context.rb
+++ spec/support/shared_contexts/authentication_shared_context.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-shared_context 'with authenticated user' do |user|
+shared_context 'with authenticated user' do
   let(:access_token) { create(:access_token, resource_owner_id: authenticated_user.id) }
   let(:authentication_headers) { { 'Authorization' => "Bearer #{access_token.token}" } }
 end
