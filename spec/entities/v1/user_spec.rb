bb5d60d30ade9d50d7c0d168377c448b2b06736d
Bartosz Suchodolski <bartosz.suchodolski@netguru.pl>
2019-12-04 13:37:27 +0100

    Do not expose users with pending invitation in household account entity

---
+++ spec/entities/v1/user_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Entities::V1::User, type: :entity do
+  subject(:entity) { described_class.new(user) }
+
+  let(:user) { create(:user) }
+
+  it 'exposes only allowed columns', :aggregate_failures do
+    expect(entity_json).to have_key(:id)
+    expect(entity_json).to have_key(:email)
+    expect(entity_json.length).to eq(2)
+  end
+end
